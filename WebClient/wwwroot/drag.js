9419d521acb6484e6ce5202dc3e6dc72eeb37b26
Otee Ziregbe <otee.ziregbe@cyberspace.net.ng>
2020-09-16 11:41:55 +0100

    Fixed Task Assign

--- WebClient/wwwroot/drag.js
+++ WebClient/wwwroot/drag.js
@@ -1,36 +1,60 @@
 ﻿
 
-function DropMe(dotnetHelper) {
+function Drag(e) {
+
+    console.log("Dragging", e.currentTarget);
+
+    const dragTaskHiddenInput = document.querySelector("#dragTaskId");
+
+    dragTaskHiddenInput.value = e.target.id;
+}
+
+function RefreshTaskForDraging() {
 
     const taskItems = document.querySelectorAll(".task-item");
 
-    taskItems.forEach(u => u.addEventListener("drag", Drag))
+    taskItems.forEach(u => { u.ondrag = null });
 
-    let taskId;
+    taskItems.forEach(u => { u.addEventListener("drag", Drag) });
 
-    function Drag(e) {
-        taskId = e.target.id;
-    }
+}
 
-    const menuItems = document.querySelectorAll(".menu-item");
+function DropMe(dotnetHelper) {
 
-    menuItems.forEach(u => u.addEventListener("drop", Drop));
+    
+    //const taskItems = document.querySelectorAll(".task-item");
+
+    //taskItems.forEach(u => { u.ondrag = null });
+
+    //taskItems.forEach(u => { u.addEventListener("drag", Drag) });
+        
+
+    const menuItems = document.querySelectorAll(".drop-menu");
+
+    menuItems.forEach(u => { u.ondrag = null });
+
+    menuItems.forEach(u => { u.addEventListener("drop", Drop); });
 
     async function Drop(e) {
+        e.stopPropagation();
+        e.preventDefault();
+        const memberId = e.currentTarget.id;
+        const taskId = document.querySelector("#dragTaskId").value;
 
-        console.log("Drop Current Target", e.currentTarget);
-        console.log("Drop Target", e.target);
-        const memberId = e.target.id;
-
+        console.log('Called Api')
         await dotnetHelper.invokeMethodAsync("AssignTaskToMember", taskId.toString(), memberId.toString())
-            
+
+        document.querySelector("#dragTaskId").value = "";
 
     }
 
     menuItems.forEach(u => u.addEventListener("dragover", allowDrop));
 
+    menuItems.forEach(u => { u.ondragover = null });
+
     function allowDrop(e) {
-        console.log("Allow drop");
+        e.stopPropagation();
+       // console.log("Allow drop");
         e.preventDefault();
     }
 
